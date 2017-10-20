953416d6775876cf284227215f13e2c430b6fe5d
hemlk <hiep@elitetech.vn>
2017-10-20 18:20:38 +0700

    apply the created sprite

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
+var rename = require('gulp-rename');
+
 var config = {
     mode: {
         css: {
@@ -15,4 +17,10 @@ gulp.task('createSprite', () => {
     return gulp.src('./app/assets/images/icons/**/*.svg')
         .pipe(svgSprite(config))
         .pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+gulp.task('copySprite', () => {
+    return gulp.src('./app/temp/sprite/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/modules'));
 });
