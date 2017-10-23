a103febc8b14ecdc781d082609be5a8277192064
hemlk <hiep@elitetech.vn>
2017-10-23 13:50:30 +0700

    adding in organised scripts

---
+++ app/assets/scripts/modules/Person.js
@@ -0,0 +1,7 @@
+function Person(fullname, fColor) {
+    this.name = fullname;
+    this.fColor = fColor;
+    this.greet = function () {
+        console.log('Hello, my name is' + this.name + ' and my f-color is: ' + this.fColor);
+    }
+}
