f1c4751611c1109e2d10b7e2bfbe5e12c42c3df3
hemlk <hiep@elitetech.vn>
2017-10-24 18:49:06 +0700

    done the Modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 let mMenu = new MobileMenu();
 //let revealOnScroll = new RevealOnScroll();
 let revealFeatures = new RevealOnScroll('.feature-item', '85%');
 let revealTesimonials = new RevealOnScroll('.testimonial', '60%');
 let stickyHeader = new StickyHeader();
+let modal = new Modal();
