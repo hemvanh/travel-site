4c4e6bca5e93f08fd9b4a0273c7aa3a072735c27
hemlk <hiep@elitetech.vn>
2017-10-26 18:31:27 +0700

    svg png for old browsers

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var webpack = require('webpack');
 var browserSync = require('browser-sync');
 
-gulp.task('scripts', (callback) => {
+gulp.task('scripts', ['modernizr'], (callback) => {
     webpack(require('../../webpack.config.js'), (err, stats) => {
         if (err) {
             console.log(err.toString());
